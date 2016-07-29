b6e4380b008ccdf21383de453104d6429ca21705
Rogier Lommers <rogier@lommers.org>
2016-07-29 08:06:19 +0200

    init

--- go-gmail-sender.go
+++ go-gmail-sender.go
@@ -1,30 +1,23 @@
 package gmail
 
-import (
-	"log"
-	"net/smtp"
-)
+import "net/smtp"
 
 func main() {
-	send("hello there")
+	sendMail("subject here", "just some testing...")
 }
 
-func send(body string) {
+func sendMail(subject, body string) error {
+	receiver := "..."
 	from := "..."
-	pass := "..."
-	to := "..."
-
+	password := "..."
 	msg := "From: " + from + "\n" +
-		"To: " + to + "\n" +
-		"Subject: Hello there\n\n" +
+		"To: " + receiver + "\n" +
+		"Subject: " + subject + "\n\n" +
 		body
 
-	err := smtp.SendMail("smtp.gmail.com:587",
-		smtp.PlainAuth("", from, pass, "smtp.gmail.com"),
-		from, []string{to}, []byte(msg))
-
+	err := smtp.SendMail("smtp.gmail.com:587", smtp.PlainAuth("", from, password, "smtp.gmail.com"), from, []string{receiver}, []byte(msg))
 	if err != nil {
-		log.Printf("smtp error: %s", err)
-		return
+		return err
 	}
+	return nil
 }
